0136119636f91ec90b588aeae50ac4df029f68bd
Maharshi Basu <basumaharshi10@gmail.com>
2024-08-10 00:34:01 +0530

    chore: clean project build cache
    Signed-off-by: Maharshi Basu <basumaharshi10@gmail.com>

--- testdata/calculator-rust/target/debug/deps/calculator_rust-5d8f70a5b43f0d47.d
+++
@@ -1,5 +0,0 @@
-/home/okabe/open-source/kaizen-experiments/testdata/calculator-rust/target/debug/deps/libcalculator_rust-5d8f70a5b43f0d47.rmeta: src/main.rs
-
-/home/okabe/open-source/kaizen-experiments/testdata/calculator-rust/target/debug/deps/calculator_rust-5d8f70a5b43f0d47.d: src/main.rs
-
-src/main.rs:
--- testdata/calculator-rust/target/debug/deps/calculator_rust-ec52cfb33dc32950.d
+++
@@ -1,5 +0,0 @@
-/home/okabe/open-source/kaizen-experiments/testdata/calculator-rust/target/debug/deps/libcalculator_rust-ec52cfb33dc32950.rmeta: src/main.rs
-
-/home/okabe/open-source/kaizen-experiments/testdata/calculator-rust/target/debug/deps/calculator_rust-ec52cfb33dc32950.d: src/main.rs
-
-src/main.rs:
